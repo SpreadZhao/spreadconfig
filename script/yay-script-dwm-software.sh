df09257f131f5f767bcf9ec8acd44bda4b32bf26
SpreadZhao <www.spreadzhao@outlook.com>
2024-05-21 01:53:18 +0800

    add to yay software script for dwm
    dunst libnotify

--- script/yay-script-dwm-software.sh
+++ script/yay-script-dwm-software.sh
@@ -1,1 +1,1 @@
-yay -S openssh flameshot base-devel lazygit scrcpy libreoffice-still obsidian android-studio code intellij-idea-community-edition obs-studio libxcrypt-compat linuxqq wechat-universal-bwrap tlp pacman-contrib ranger
+yay -S openssh flameshot base-devel lazygit scrcpy libreoffice-still obsidian android-studio code intellij-idea-community-edition obs-studio libxcrypt-compat linuxqq wechat-universal-bwrap tlp pacman-contrib ranger alacritty dunst libnotify
