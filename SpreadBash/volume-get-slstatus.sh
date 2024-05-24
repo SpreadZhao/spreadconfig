649d3e8f71a24ca5e52fc72fc34f47e2f8273bf3
SpreadZhao <www.spreadzhao@outlook.com>
2024-05-25 00:41:47 +0800

    pamixer get volume sh
    do not forget #! /bin/bash at the begining!!!!!!!

--- SpreadBash/volume-get-slstatus.sh
+++ SpreadBash/volume-get-slstatus.sh
@@ -1,2 +1,11 @@
-VOL="🔈$(amixer get Master | tail -1 | sed 's/.*\[\([0-9]*%\)\].*/\1/')"
-echo $VOL
+#! /bin/bash
+VOL=$(amixer sget Master | tail -1 | awk '{print $5 }' | sed "s@\\(\\[\\|\\]\\)@@g")
+VOLSTATUS=$(amixer sget Master | tail -1 | awk '{print $6 }' | sed "s@\\(\\[\\|\\]\\)@@g")
+if [ "$VOLSTATUS" == "on" ]; then
+    RES="󰕾 $VOL"
+else
+    RES="󰖁 $VOL"
+fi
+# echo "$RES"
+# VOL="🔈$(amixer get Master | tail -1 | sed 's/.*\[\([0-9]*%\)\].*/\1/')"
+printf '%s\n' "$RES"
