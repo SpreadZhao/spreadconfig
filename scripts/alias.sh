0ee55bf02701821be13fc2227dafeefd884ce034
SpreadZhao <spreadzhao@outlook.com>
2025-08-01 01:32:24 +0800

    Add scripts

---
+++ scripts/alias.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+alias n="nvim ."
+alias wk="cd ~/workspaces"
+alias nh="cd ~/workspaces/obsidianws/notes_homeworks"
+alias st="cd ~/workspaces/SpreadStudy/"
+alias lg="lazygit"
+alias ls="eza --icons"
+alias ll="eza -l --icons"
+alias la="eza -la --icons"
+alias l="eza -lah --icons"
+alias tt="tray-tui"
