9a9b486771cee4ed6884cefb174422a922f79a1b
SpreadZhao <spreadzhao@outlook.com>
2025-10-22 22:57:25 +0800

    delete env

--- NiriConfig_AMD/scripts/env.sh
+++ NiriConfig_AMD/scripts/env.sh
@@ -3,12 +3,12 @@
 export EDITOR=nvim
 export VISUAL=nvim
 
-export XDG_SESSION_DESKTOP="sway"
-if [ "$XDG_SESSION_DESKTOP" = "sway" ]; then
-    # https://github.com/swaywm/sway/issues/595
-    export _JAVA_AWT_WM_NONREPARENTING=1
-fi
-export _JAVA_AWT_WM_NONREPARENTING=1
+# export XDG_SESSION_DESKTOP="sway"
+# if [ "$XDG_SESSION_DESKTOP" = "sway" ]; then
+#     # https://github.com/swaywm/sway/issues/595
+#     export _JAVA_AWT_WM_NONREPARENTING=1
+# fi
+# export _JAVA_AWT_WM_NONREPARENTING=1
 
 # PATH
 append_path() {
