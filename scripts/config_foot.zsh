8955ad3bf111acde81fa8b02d9ebbb119fa668ff
SpreadZhao <spreadzhao@outlook.com>
2025-12-27 18:41:59 +0800

    foot: show title

---
+++ scripts/config_foot.zsh
@@ -0,0 +1,18 @@
+# https://codeberg.org/dnkl/foot/wiki#spawning-new-terminal-instances-in-the-current-working-directory
+function osc7-pwd() {
+    emulate -L zsh # also sets localoptions for us
+    setopt extendedglob
+    local LC_ALL=C
+    printf '\e]7;file://%s%s\e\' $HOST ${PWD//(#m)([^@-Za-z&-;_~])/%${(l:2::0:)$(([##16]#MATCH))}}
+}
+
+function chpwd-osc7-pwd() {
+    (( ZSH_SUBSHELL )) || osc7-pwd
+}
+add-zsh-hook -Uz chpwd chpwd-osc7-pwd
+
+# show curr program on title
+# https://codeberg.org/dnkl/foot/issues/242
+function preexec {
+    print -Pn "\e]0;${(q)1}(foot)\e\\"
+}
