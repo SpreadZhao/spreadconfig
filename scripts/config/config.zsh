260332d4d0b69a195ec2d2527025a80372f3fc5c
SpreadZhao <spreadzhao@outlook.com>
2026-01-03 00:19:58 +0800

    approximate completion

--- scripts/config/config.zsh
+++ scripts/config/config.zsh
@@ -4,6 +4,5 @@ source "$ZSH_CONFIG_DIR/env.sh"
 source "$ZSH_CONFIG_DIR/alias.sh"
 source "$ZSH_CONFIG_DIR/color_output.sh"
 source "$ZSH_CONFIG_DIR/config_zsh.zsh"
-# source "$ZSH_CONFIG_DIR/config_jj.zsh"
 
 unset -v ZSH_CONFIG_DIR
--- scripts/config/config_zsh.zsh
+++ scripts/config/config_zsh.zsh
@@ -218,6 +218,7 @@ config_fzf_tab() {
     zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
     # force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
     zstyle ':completion:*' menu no
+    zstyle ':completion:*' completer _extensions _complete _approximate
     # preview directory's content with eza when completing cd
     zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
     # custom fzf flags
