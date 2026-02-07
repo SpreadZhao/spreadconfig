05dd22e3c95eee6f93352df9b0092d491c89355b
SpreadZhao <spreadzhao@outlook.com>
2026-02-07 21:55:03 +0800

    ollama!!! niubi stuff

--- home.nix
+++ home.nix
@@ -2196,6 +2196,13 @@ in
         };
     };
     services = {
+        ollama = {
+            enable = true;
+            package = pkgs.ollama-rocm;
+            acceleration = "rocm";
+            host = "127.0.0.1";
+            port = 11434;
+        };
         cliphist = {
             enable = true;
             extraOptions = [
