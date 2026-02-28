73189f9e4c4207c25f1fd917a3e56d5673a0d698
SpreadZhao <spreadzhao@outlook.com>
2026-03-01 01:56:05 +0800

    nixvim: add yamlls

--- home.nix
+++ home.nix
@@ -2729,6 +2729,33 @@ in
                             '';
                         };
                     };
+                    yamlls = {
+                        enable = true;
+                        config = {
+                            cmd = [
+                                "yaml-language-server"
+                                "--stdio"
+                            ];
+                            filetypes = [
+                                "yaml"
+                                "yaml.docker-compose"
+                                "yaml.gitlab"
+                                "yaml.helm-values"
+                            ];
+                            root_markers = [
+                                ".git"
+                            ];
+                            settings = {
+                                redhat.telemetry.enabled = false;
+                                yaml.format.enable = true;
+                            };
+                            on_init.__raw = ''
+                                function(client)
+                                    client.server_capabilities.documentFormattingProvider = true
+                                end
+                            '';
+                        };
+                    };
                 };
             };
         };
