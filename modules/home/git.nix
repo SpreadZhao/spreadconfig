21a92c4bf85bf3447bda3c51b9ecdc86d63c23e1
SpreadZhao <spreadzhao@outlook.com>
2026-05-25 01:44:29 +0800

    chore(git): install git as home package
    - Simplifies the git home module to provide the binary only
    - Leaves identity and editor configuration out of this module

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,16 +1,5 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  programs.git = {
-    enable = true;
-    settings = {
-      user = {
-        name = "SpreadZhao";
-        email = "spreadzhao@outlook.com";
-      };
-      core = {
-        editor = "nvim";
-      };
-    };
-  };
+  home.packages = [ pkgs.git ];
 }
