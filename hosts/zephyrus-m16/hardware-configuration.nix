be02a672c4f6793da43d26925c4fb5aaf29c59b8
SpreadZhao <spreadzhao@outlook.com>
2026-05-28 00:48:50 +0800

    feat(nixos): add zephyrus data mount
    - Mount /data as an optional ext4 partition
    - Prepare the user data directory through tmpfiles

--- hosts/zephyrus-m16/hardware-configuration.nix
+++ hosts/zephyrus-m16/hardware-configuration.nix
@@ -22,21 +22,35 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/d7af7009-dfb2-4c95-baf2-4df6d1a73807";
-    fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/C446-F9DE";
-    fsType = "vfat";
-    options = [
-      "fmask=0077"
-      "dmask=0077"
-    ];
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/d7af7009-dfb2-4c95-baf2-4df6d1a73807";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/C446-F9DE";
+      fsType = "vfat";
+      options = [
+        "fmask=0077"
+        "dmask=0077"
+      ];
+    };
+    "/data" = {
+      device = "/dev/disk/by-uuid/525ef6ef-0aa2-4732-969b-ca9aef4dcf56";
+      fsType = "ext4";
+      options = [
+        "nofail"
+        "x-systemd.device-timeout=10s"
+      ];
+    };
   };
 
   swapDevices = [ ];
+
+  # only need when /data was mounted
+  systemd.tmpfiles.rules = [
+    "d /data/spreadzhao 0755 spreadzhao users -"
+  ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
