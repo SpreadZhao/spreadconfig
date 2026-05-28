b5d6f7af33f85cbe8a9e0ffba9324ddba6fed997
SpreadZhao <spreadzhao@outlook.com>
2026-05-29 00:51:58 +0800

    fix(qutebrowser): use nvidia offload on zephyrus
    - Add host-specific desktop exec for zephyrus-m16
    - Preserve qt scaling for other qutebrowser hosts

--- modules/home/qutebrowser.nix
+++ modules/home/qutebrowser.nix
@@ -1,10 +1,20 @@
 {
   config,
   hostConfigSource,
+  hostName,
+  lib,
   pkgsPinned,
   secretsDir,
   ...
 }:
+
+let
+  qutebrowserDesktopExec =
+    if hostName == "zephyrus-m16" then
+      "Exec=env QT_SCALE_FACTOR=1.5 __NV_PRIME_RENDER_OFFLOAD=1 __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only qutebrowser"
+    else
+      "Exec=env QT_SCALE_FACTOR=1.5 qutebrowser";
+in
 
 {
   home.packages = [
@@ -12,7 +22,7 @@
       (pkgsPinned.old_a6c3b1b.qutebrowser.overrideAttrs (old: {
         postInstall = (old.postInstall or "") + ''
           substituteInPlace $out/share/applications/org.qutebrowser.qutebrowser.desktop \
-            --replace-fail "Exec=qutebrowser" "Exec=env QT_SCALE_FACTOR=1.5 qutebrowser"
+            --replace-fail "Exec=qutebrowser" ${lib.escapeShellArg qutebrowserDesktopExec}
         '';
       })).override
       {
