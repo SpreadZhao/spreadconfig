3b73f7f658621a2d825a4864fd69db67866fdcd7
SpreadZhao <spreadzhao@outlook.com>
2026-06-03 01:07:23 +0800

    git config

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,5 +1,19 @@
-{ pkgs, ... }:
-
 {
-  home.packages = [ pkgs.git ];
+  programs.git = {
+    enable = true;
+    userName = "SpreadZhao";
+    userEmail = "spreadzhao@outlook.com";
+    settings = {
+      credential = {
+        "https://github.com".helper = [
+          ""
+          "!gh auth git-credential"
+        ];
+        "https://gist.github.com".helper = [
+          ""
+          "!gh auth git-credential"
+        ];
+      };
+    };
+  };
 }
