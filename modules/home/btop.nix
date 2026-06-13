f4da9fac327f86875ead93576031ad36f538f0f7
SpreadZhao <spreadzhao@outlook.com>
2026-06-13 17:10:54 +0800

    feat(btop): add host gpu settings
    - Configure btop GPU display per host
    - Wrap btop with OpenGL library path for NVIDIA host

--- modules/home/btop.nix
+++ modules/home/btop.nix
@@ -1,12 +1,44 @@
-{ ... }:
+{
+  hostName,
+  pkgs,
+  ...
+}:
 
+let
+  baseSettings = {
+    theme_background = false;
+    truecolor = true;
+    vim_keys = true;
+  };
+
+  gpuSettingsByHost = {
+    thinkbook = {
+      shown_gpus = "amd";
+    };
+
+    zephyrus-m16 = {
+      shown_boxes = "cpu mem net proc gpu0";
+      shown_gpus = "nvidia";
+      show_gpu_info = "On";
+    };
+  };
+
+  nvidiaBtop = pkgs.symlinkJoin {
+    name = "btop-nvidia";
+    paths = [ pkgs.btop ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/btop \
+        --prefix LD_LIBRARY_PATH : /run/opengl-driver/lib
+    '';
+  };
+
+  hostGpuSettings = gpuSettingsByHost.${hostName} or { };
+in
 {
   programs.btop = {
     enable = true;
-    settings = {
-      theme_background = false;
-      truecolor = true;
-      vim_keys = true;
-    };
+    package = if hostName == "zephyrus-m16" then nvidiaBtop else pkgs.btop;
+    settings = baseSettings // hostGpuSettings;
   };
 }
