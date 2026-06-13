dea175fc69e15c5dcf9578bc8e5a0f5def23ac5b
SpreadZhao <spreadzhao@outlook.com>
2026-06-13 15:31:01 +0800

    feat(nix): propagate proxy env vars
    - Export proxy environment variables when a default proxy is configured
    - Pass no_proxy and npm proxy settings through for Nix tooling

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -9,8 +9,33 @@
   nix =
     let
       flakeInputs = lib.filterAttrs (name: input: name != "self" && lib.isType "flake" input) inputs;
+      proxy = config.networking.proxy.default or "";
+      noProxy = config.networking.proxy.noProxy or "";
     in
     {
+      envVars = lib.mkIf (proxy != "") {
+        all_proxy = proxy;
+        ftp_proxy = proxy;
+        http_proxy = proxy;
+        https_proxy = proxy;
+        rsync_proxy = proxy;
+        no_proxy = noProxy;
+
+        ALL_PROXY = proxy;
+        FTP_PROXY = proxy;
+        HTTP_PROXY = proxy;
+        HTTPS_PROXY = proxy;
+        RSYNC_PROXY = proxy;
+        NO_PROXY = noProxy;
+
+        npm_config_proxy = proxy;
+        npm_config_https_proxy = proxy;
+        npm_config_noproxy = noProxy;
+        NPM_CONFIG_PROXY = proxy;
+        NPM_CONFIG_HTTPS_PROXY = proxy;
+        NPM_CONFIG_NOPROXY = noProxy;
+      };
+
       settings = {
         # Enable flakes and new 'nix' command
         experimental-features = "nix-command flakes";
