f70eb7a5fbea92142cd615f378d81e33c0f5d6f7
SpreadZhao <spreadzhao@outlook.com>
2026-07-19 23:27:09 +0800

    feat: use Nyxt 4 Electron renderer

---
+++ modules/home/nyxt.nix
@@ -0,0 +1,27 @@
+{
+  hostConfigSource,
+  inputs,
+  pkgs,
+  ...
+}:
+
+let
+  personalPackages = inputs.personal-packages.packages.${pkgs.stdenv.hostPlatform.system};
+  nyxt4 = pkgs.symlinkJoin {
+    name = "nyxt4-profile";
+    paths = [ personalPackages.nyxt4 ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      rm "$out/bin/nyxt"
+      makeWrapper "${personalPackages.nyxt4}/bin/nyxt" "$out/bin/nyxt" \
+        --run 'export XDG_DATA_HOME="''${XDG_DATA_HOME:-$HOME/.local/share}/nyxt4"' \
+        --run 'export XDG_CACHE_HOME="''${XDG_CACHE_HOME:-$HOME/.cache}/nyxt4"'
+    '';
+    inherit (personalPackages.nyxt4) meta;
+  };
+in
+{
+  home.packages = [ nyxt4 ];
+
+  xdg.configFile."nyxt/config.lisp".source = hostConfigSource "nyxt/config.lisp";
+}
