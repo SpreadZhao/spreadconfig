6a52127cc4c0d696e45e3d571caec8a26dc52bf9
SpreadZhao <spreadzhao@outlook.com>
2026-07-24 22:42:49 +0800

    chore(nix): update inputs and tighten auth
    - Updated flake locks for agent and system inputs
    - Disabled Hermes Agent package and desktop entry wiring
    - Removed passwordless wheel polkit and sudo access

--- modules/home/hermes-agent.nix
+++ modules/home/hermes-agent.nix
@@ -4,21 +4,21 @@ let
   hermesPackages = inputs.hermes-agent.packages.${pkgs.stdenv.hostPlatform.system};
 in
 {
-  home.packages = [
-    hermesPackages.default
-    hermesPackages.desktop
-  ];
-
-  xdg.desktopEntries.hermes-desktop = {
-    name = "Hermes Desktop";
-    comment = "Native desktop shell for Hermes Agent";
-    exec = "${hermesPackages.desktop}/bin/hermes-desktop";
-    icon = "utilities-terminal";
-    categories = [
-      "Development"
-      "Utility"
-    ];
-    terminal = false;
-    type = "Application";
-  };
+  # home.packages = [
+  #   hermesPackages.default
+  #   hermesPackages.desktop
+  # ];
+  #
+  # xdg.desktopEntries.hermes-desktop = {
+  #   name = "Hermes Desktop";
+  #   comment = "Native desktop shell for Hermes Agent";
+  #   exec = "${hermesPackages.desktop}/bin/hermes-desktop";
+  #   icon = "utilities-terminal";
+  #   categories = [
+  #     "Development"
+  #     "Utility"
+  #   ];
+  #   terminal = false;
+  #   type = "Application";
+  # };
 }
--- modules/nixos/security.nix
+++ modules/nixos/security.nix
@@ -2,16 +2,7 @@
 
 {
   security = {
-    polkit = {
-      enable = true;
-      # https://wiki.nixos.org/wiki/Polkit#No_password_for_wheel
-      extraConfig = ''
-        polkit.addRule(function(action, subject) {
-            if (subject.isInGroup("wheel"))
-                return polkit.Result.YES;
-        });
-      '';
-    };
+    polkit.enable = true;
     pam.services = {
       # https://wiki.nixos.org/wiki/Secret_Service#Auto-decrypt_on_login
       # login.enableGnomeKeyring = true;
@@ -19,6 +10,6 @@
       # https://wiki.nixos.org/wiki/Swaylock#Home_Manager_-_Through_Sway
       swaylock = { };
     };
-    sudo.wheelNeedsPassword = false;
+    sudo.wheelNeedsPassword = true;
   };
 }
