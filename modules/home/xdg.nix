581e16c5420e9b80926ea82346d63d222a4ae71c
SpreadZhao <spreadzhao@outlook.com>
2026-08-12 21:30:14 +0800

    feat(xdg): expand default app associations
    - Centralize MIME and URI handler mappings by application
    - Register draw.io files and broader format support
    - Ensure desktop packages expose their MIME associations

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -1,5 +1,6 @@
 {
   config,
+  lib,
   pkgs,
   scriptsDir,
   spreadconfigDir,
@@ -13,8 +14,183 @@ let
     Name=Fcitx 5
     Hidden=true
   '';
+
+  drawioMimePackage = pkgs.writeTextDir "share/mime/packages/drawio.xml" ''
+    <?xml version="1.0" encoding="UTF-8"?>
+    <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+      <mime-type type="application/vnd.jgraph.mxfile">
+        <comment>draw.io diagram</comment>
+        <glob pattern="*.drawio" weight="80"/>
+        <glob pattern="*.drawio.xml" weight="80"/>
+      </mime-type>
+    </mime-info>
+  '';
+
+  desktopApps = {
+    antigravity = "antigravity.desktop";
+    browser = "org.qutebrowser.qutebrowser.desktop";
+    chromium = "chromium-browser.desktop";
+    clash = "clash-verge.desktop";
+    codex = "codex-desktop.desktop";
+    drawio = "drawio.desktop";
+    editor = "nvim.desktop";
+    element = "element-desktop.desktop";
+    feishu = "bytedance-feishu.desktop";
+    fileManager = "lf.desktop";
+    obsidian = "obsidian.desktop";
+    rnote = "com.github.flxzt.rnote.desktop";
+    swayimg = "swayimg.desktop";
+    telegram = "org.telegram.desktop.desktop";
+    waydroidInstaller = "waydroid.app.install.desktop";
+    waydroidMarket = "waydroid.market.desktop";
+    wemeet = "wemeetapp.desktop";
+    xournal = "com.github.xournalpp.xournalpp.desktop";
+    zathura = "org.pwmt.zathura.desktop";
+    zcode = "zcode.desktop";
+  };
+
+  defaultsFor = application: mimeTypes: lib.genAttrs mimeTypes (_: application);
+
+  defaultApplications =
+    defaultsFor desktopApps.editor [
+      "application/json"
+      "application/schema+json"
+      "application/sql"
+      "application/toml"
+      "application/x-ndjson"
+      "application/x-shellscript"
+      "application/xml"
+      "application/yaml"
+      "text/css"
+      "text/javascript"
+      "text/markdown"
+      "text/plain"
+      "text/rust"
+      "text/vnd.trolltech.linguist"
+      "text/x-c++hdr"
+      "text/x-c++src"
+      "text/x-chdr"
+      "text/x-cmake"
+      "text/x-csrc"
+      "text/x-go"
+      "text/x-java"
+      "text/x-kotlin"
+      "text/x-lua"
+      "text/x-makefile"
+      "text/x-python"
+      "text/x-python3"
+      "text/x-tex"
+      "text/xml"
+      "text/x-yaml"
+      "text/yaml"
+    ]
+    // defaultsFor desktopApps.browser [
+      "application/xhtml+xml"
+      "text/html"
+      "x-scheme-handler/about"
+      "x-scheme-handler/http"
+      "x-scheme-handler/https"
+      "x-scheme-handler/qute"
+      "x-scheme-handler/unknown"
+    ]
+    // defaultsFor desktopApps.zathura [
+      "application/pdf"
+    ]
+    // defaultsFor desktopApps.swayimg [
+      "image/avif"
+      "image/bmp"
+      "image/gif"
+      "image/heic"
+      "image/heif"
+      "image/jpeg"
+      "image/jpg"
+      "image/jxl"
+      "image/pbm"
+      "image/pjpeg"
+      "image/png"
+      "image/svg+xml"
+      "image/tiff"
+      "image/webp"
+      "image/x-bmp"
+      "image/x-exr"
+      "image/x-pcx"
+      "image/x-png"
+      "image/x-portable-anymap"
+      "image/x-portable-bitmap"
+      "image/x-portable-graymap"
+      "image/x-portable-pixmap"
+      "image/x-targa"
+      "image/x-tga"
+      "image/x-xbitmap"
+    ]
+    // defaultsFor desktopApps.drawio [
+      "application/vnd.jgraph.mxfile"
+      "application/vnd.ms-visio.drawing.macroEnabled.main+xml"
+      "application/vnd.ms-visio.drawing.main+xml"
+      "application/vnd.ms-visio.stencil.macroEnabled.main+xml"
+      "application/vnd.ms-visio.stencil.main+xml"
+      "application/vnd.ms-visio.template.macroEnabled.main+xml"
+      "application/vnd.ms-visio.template.main+xml"
+      "application/vnd.visio"
+    ]
+    // defaultsFor desktopApps.rnote [
+      "application/rnote"
+    ]
+    // defaultsFor desktopApps.xournal [
+      "application/x-xoj"
+      "application/x-xojpp"
+      "application/x-xopp"
+      "application/x-xopt"
+    ]
+    // defaultsFor desktopApps.fileManager [
+      "inode/directory"
+    ]
+    // defaultsFor desktopApps.waydroidInstaller [
+      "application/vnd.android.package-archive"
+    ]
+    // defaultsFor desktopApps.antigravity [
+      "x-scheme-handler/antigravity"
+    ]
+    // defaultsFor desktopApps.chromium [
+      "x-scheme-handler/chromium"
+    ]
+    // defaultsFor desktopApps.clash [
+      "x-scheme-handler/clash"
+    ]
+    // defaultsFor desktopApps.codex [
+      "x-scheme-handler/codex"
+      "x-scheme-handler/codex-browser-sidebar"
+    ]
+    // defaultsFor desktopApps.element [
+      "x-scheme-handler/element"
+      "x-scheme-handler/io.element.desktop"
+    ]
+    // defaultsFor desktopApps.feishu [
+      "x-scheme-handler/feishu"
+      "x-scheme-handler/feishu-open"
+      "x-scheme-handler/lark"
+      "x-scheme-handler/x-feishu"
+    ]
+    // defaultsFor desktopApps.obsidian [
+      "x-scheme-handler/obsidian"
+    ]
+    // defaultsFor desktopApps.telegram [
+      "x-scheme-handler/tg"
+      "x-scheme-handler/tonsite"
+    ]
+    // defaultsFor desktopApps.waydroidMarket [
+      "x-scheme-handler/market"
+    ]
+    // defaultsFor desktopApps.wemeet [
+      "x-scheme-handler/wemeet"
+    ]
+    // defaultsFor desktopApps.zcode [
+      "x-scheme-handler/zcode"
+    ];
 in
 {
+  home.packages = [ drawioMimePackage ];
+
   xdg = {
     enable = true;
     autostart = {
@@ -91,41 +267,22 @@ in
     mime.enable = true;
     mimeApps = {
       enable = true;
-      defaultApplications = {
-        "text/html" = "org.qutebrowser.qutebrowser.desktop";
-        "application/pdf" = "org.pwmt.zathura.desktop";
-        "image/bmp" = "swayimg.desktop";
-        "image/gif" = "swayimg.desktop";
-        "image/jpeg" = "swayimg.desktop";
-        "image/jpg" = "swayimg.desktop";
-        "image/pjpeg" = "swayimg.desktop";
-        "image/png" = "swayimg.desktop";
-        "image/tiff" = "swayimg.desktop";
-        "image/webp" = "swayimg.desktop";
-        "image/x-bmp" = "swayimg.desktop";
-        "image/x-pcx" = "swayimg.desktop";
-        "image/x-png" = "swayimg.desktop";
-        "image/x-portable-anymap" = "swayimg.desktop";
-        "image/x-portable-bitmap" = "swayimg.desktop";
-        "image/x-portable-graymap" = "swayimg.desktop";
-        "image/x-portable-pixmap" = "swayimg.desktop";
-        "image/x-tga" = "swayimg.desktop";
-        "image/x-xbitmap" = "swayimg.desktop";
-        "image/heic" = "swayimg.desktop";
-        "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
-        "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
-        "x-scheme-handler/about" = "org.qutebrowser.qutebrowser.desktop";
-        "x-scheme-handler/unknown" = "org.qutebrowser.qutebrowser.desktop";
-        "x-scheme-handler/antigravity" = "antigravity.desktop";
-        "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
-        "x-scheme-handler/tonsite" = "org.telegram.desktop.desktop";
-      };
-      associations.added = {
-        "application/pdf" = "org.pwmt.zathura.desktop";
-        "x-scheme-handler/antigravity" = "antigravity.desktop";
-        "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
-        "x-scheme-handler/tonsite" = "org.telegram.desktop.desktop";
-      };
+      defaultApplicationPackages = with pkgs; [
+        swayimg
+        mpv
+        libreoffice-still
+        drawio
+        rnote
+        xournalpp
+      ];
+      inherit defaultApplications;
+      associations.added =
+        defaultsFor desktopApps.zathura [ "application/pdf" ]
+        // defaultsFor desktopApps.antigravity [ "x-scheme-handler/antigravity" ]
+        // defaultsFor desktopApps.telegram [
+          "x-scheme-handler/tg"
+          "x-scheme-handler/tonsite"
+        ];
     };
     portal = {
       enable = true;
